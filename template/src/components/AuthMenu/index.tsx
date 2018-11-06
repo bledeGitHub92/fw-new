13ccc56ba901982794aa7df8a68549b6d34d1b02
兰星 <bluestar@lanxingdeiMac.local>
2018-11-06 11:01:11 +0800

    调整 AuthMenu 代码格式

--- template/src/components/AuthMenu/index.tsx
+++ template/src/components/AuthMenu/index.tsx
@@ -33,21 +33,19 @@ class AuthMenu extends Component {
   componentDidUpdate (prevProps, prevState) {
     if (prevProps.menus !== this.injected.menus) {
       this.setState({ next: false, });
-      this.getNextRoute(this.injected.menus);
+      this.getNextRoute();
+      this.setState({ next: true, });
+
     }
   }
 
-  getNextRoute (menus) {
+  getNextRoute () {
+    const { menus, dispatch, location } = this.injected
     const isDev = process.env.NODE_ENV === 'development';
     const DEV_HOST = process.env.DEV_HOST;
-    const { dispatch, location } = this.injected
-    const hostname = isDev ? DEV_HOST : window.location.hostname;
     const isNoPower = menus.length === 0;
+    const hostname = isDev ? DEV_HOST : window.location.hostname;
     const currHost = menus.find(({ domain }) => domain === hostname);
-
-    this.setState({
-      next: true,
-    });
 
     if (isNoPower) {
       return dispatch(routerRedux.replace('/403'));
