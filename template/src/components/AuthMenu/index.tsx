d104169dacbead6e8685e9a32361888e6e526a09
兰星 <bluestar@lanxingdeiMac.local>
2018-11-28 20:13:41 +0800

    fix 404

--- template/src/components/AuthMenu/index.tsx
+++ template/src/components/AuthMenu/index.tsx
@@ -1,7 +1,8 @@
 import React, { Props, Component } from 'react';
 import { connect } from 'dva';
-import { common } from '@/interface/common';
 import { routerRedux } from 'dva/router';
+import { message } from 'antd';
+import { common } from '@/interface/common';
 import PageLoading from '@/components/PageLoading';
 
 interface Injected extends common.ConnectProps, Props<undefined> {
@@ -45,19 +46,20 @@ class AuthMenu extends Component {
     const isNoPower = menus.length === 0;
     const hostname = isDev ? DEV_HOST : window.location.hostname;
     const currHost = menus.find(({ domain }) => domain === hostname);
-    const isRedirect = currHost.children.length === 0;
 
     if (isNoPower) {
       return dispatch(routerRedux.replace('/403'));
     }
 
     const targetMenu = currHost && currHost.children.find(({ path }) => path.split('#')[1] === location.pathname);
+    const isRedirect = !targetMenu;
     const targetPath = targetMenu
       ? targetMenu.path
       : currHost
         ? currHost.children[0].path
         : menus[0].children[0].path;
 
+    message.info(targetPath);
     window.location.href = targetPath;
     return isRedirect;
   }
