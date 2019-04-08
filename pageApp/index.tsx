6e0b5f473f8801ef4d49ba55dff7ed04e4916883
兰星 <bluestar@lanxingdeiMac.local>
2019-04-08 15:10:45 +0800

    切换标题栏

--- pageApp/index.tsx
+++ pageApp/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { NavigationProps, NOConfigProps } from 'src/interfaces';
-import { NavigationStackScreenOptions } from 'react-navigation';
+import { NavigationProps } from 'src/interfaces';
 import { inject, observer } from 'mobx-react/native';
 import { FooStore } from './store';
 import { PullRenderItem } from 'components/PullRefresh/interface';
 import PullFlatList from 'components/PullRefresh/PullFlatList';
+import { Provider } from 'components/elements@2.0';
 
 interface InjectedProps extends NavigationProps {
   FooStore: FooStore,
@@ -18,10 +18,6 @@ interface State {
 @inject('FooStore')
 @observer
 class Foo extends React.Component {
-  static navigationOptions = ({ navigation }: NOConfigProps): NavigationStackScreenOptions => ({
-    title: '标题'
-  });
-
   get injected() {
     return this.props as InjectedProps;
   }
@@ -32,7 +28,11 @@ class Foo extends React.Component {
 
   render() {
     return (
-      <View />
+      <Provider headerTitle="">
+        <View style={{ flex: 1, backgroundColor: '#f4f6fa' }}>
+          
+        </View>
+      </Provider>
     );
   }
 }
